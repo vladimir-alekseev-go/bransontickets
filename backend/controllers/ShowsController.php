1776ef9ed0132977b01d9c718455e0a4c3581e8b
cuooore <82507960+BogdanKhalimon@users.noreply.github.com>
2023-08-16 13:04:07 +0300

    fix

--- backend/controllers/ShowsController.php
+++ backend/controllers/ShowsController.php
@@ -60,9 +60,6 @@ class ShowsController extends CrudController
 
             $uploadItemsBanner->file = UploadedFile::getInstance($uploadItemsBanner, 'file');
 
-            var_dump($uploadItemsBanner->validate());
-            var_dump($uploadItemsBanner->upload());
-            exit();
             if ($uploadItemsBanner->validate() && $uploadItemsBanner->upload()) {
                 if (!empty($model->image)) {
                     $model->image->delete();
