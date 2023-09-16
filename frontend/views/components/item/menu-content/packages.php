de376c573fa013ca1f748e4f339534ac5914e779
cuooore <82507960+BogdanKhalimon@users.noreply.github.com>
2023-09-16 20:12:34 +0300

    fix

--- frontend/views/components/item/menu-content/packages.php
+++ frontend/views/components/item/menu-content/packages.php
@@ -8,37 +8,3 @@
         </div>
     </div>
 </div>
-
-<?php $this->registerJs(
-    "
-    $('.view-more').click(function () {
-        $('.more').css('display', 'flex');
-        $('.view').css('display', 'none');
-    });
-
-    $('.package-1-show').click(function () {
-        $('.js-package-1').css('display', 'block');
-        $('.package-1-show').css('display', 'none');
-        $('.package-1-hide').css('display', 'block');
-    });
-
-    $('.package-1-hide').click(function () {
-        $('.js-package-1').css('display', 'none');
-        $('.package-1-hide').css('display', 'none');
-        $('.package-1-show').css('display', 'block');
-    });
-
-    $('.package-2-show').click(function () {
-        $('.js-package-2').css('display', 'block');
-        $('.package-2-show').css('display', 'none');
-        $('.package-2-hide').css('display', 'block');
-    });
-
-    $('.package-2-hide').click(function () {
-        $('.js-package-2').css('display', 'none');
-        $('.package-2-hide').css('display', 'none');
-        $('.package-2-show').css('display', 'block');
-    });
-    "
-);
-?>
