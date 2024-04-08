2254ddf40e5d8debcba8965f1e9d2922d683cee1
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2024-04-09 02:42:20 +0300

    TRPM-4479 - 1

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,10 +4,66 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About us';
+$this->title = 'Branson Getaways Made Easy with BransonTickets.com';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="fixed">
-    <h1><b><?= Html::encode($this->title) ?></b></h1>
+    <h1 class="mb-4 mb-md-5"><b><?= Html::encode($this->title) ?></b></h1>
 
+    <p class="mb-4 mb-md-5">
+        <b>
+            At BransonTickets.com, our mission is simple: create unforgettable vacation experiences for families.
+        </b>
+    </p>
+    <p class="mb-4 mb-md-5">
+        Branson offers a world of entertainment and fun, and we understand planning a trip can feel overwhelming. That's
+        where our team of Branson experts comes in! Our friendly Branson Travel Specialists, all Branson locals
+        themselves, are passionate about showing you the best this town has to offer.
+        <b>
+            Let us take care of everything, from finding the perfect shows and to securing the top attractions.
+        </b>
+        We'll work with you to craft a vacation that's both fun and memorable, all within your budget.
+    </p>
+    <p>
+        <b>Here's what sets BransonTickets.com apart:</b>
+    </p>
+
+    <ul>
+        <li><p>
+            <b>Local Expertise:</b> Our Branson Travel Specialists live and breathe Branson, ensuring you get insider
+            tips and recommendations.
+            </p></li>
+        <li><p>
+            <b>Exceptional Customer Service:</b> We prioritize your needs and wants, going the extra mile to create a
+            stress-free vacation experience.
+            </p></li>
+        <li><p>
+            <b>24/7 Convenience:</b> Access Branson information, schedules, discounts, and book your entire trip online,
+            anytime, anywhere.
+            </p></li>
+        <li><p>
+            <b>Great Value:</b> We offer bundle packages, discounts, and booking solutions to help you maximize your
+            vacation budget.
+            </p></li>
+        <li><p class="mb-4 mb-md-5">
+            <b>Integrity:</b> We believe in honesty and transparency, ensuring you have a wonderful Branson experience.
+            </p></li>
+    </ul>
+
+    <p class="mb-4 mb-md-5">
+        <b>
+            Ready to start planning your dream Branson vacation? Give us a call at 417-334-4814 or visit
+            BransonTickets.com
+            today!
+        </b>
+    </p>
+    <p class="mb-4 mb-md-5">
+        <b>
+            #BransonVacation #FamilyFun #UnforgettableMemories
+        </b>
+    </p>
+    <p class="mb-5">
+        <b>P.S.</b> We appreciate your business and hope you have such a fantastic vacation that you'll be back for
+        more!
+    </p>
 </div>
