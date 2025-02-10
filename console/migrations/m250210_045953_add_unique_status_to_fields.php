adb974a2829a9351955df389bf3ddc8f56b6125e
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2025-02-10 07:16:37 +0200

    Add unique status to fields

---
+++ console/migrations/m250210_045953_add_unique_status_to_fields.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m250210_045953_add_unique_status_to_fields
+ */
+class m250210_045953_add_unique_status_to_fields extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->alterColumn('tr_admissions', 'id_external', $this->integer()->notNull()->unique());
+        $this->alterColumn('tr_categories', 'id_external', $this->integer()->notNull()->unique());
+        $this->alterColumn('tr_pos_room_types', 'id_external', $this->integer()->notNull()->unique());
+        $this->alterColumn('vacation_package', 'vp_external_id', $this->integer()->notNull()->unique());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+    }
+}
