7c7ddcc794c2f5f2cce941d5722817d04ab73314
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2025-02-12 16:42:57 +0200

    fix

--- console/migrations/m250210_045953_add_unique_status_to_fields.php
+++ console/migrations/m250210_045953_add_unique_status_to_fields.php
@@ -12,9 +12,9 @@ class m250210_045953_add_unique_status_to_fields extends Migration
      */
     public function safeUp()
     {
-        $this->alterColumn('tr_admissions', 'id_external', $this->integer()->notNull()->unique());
+        $this->alterColumn('tr_admissions', 'id_external', $this->string(36)->notNull()->unique());
         $this->alterColumn('tr_categories', 'id_external', $this->integer()->notNull()->unique());
-        $this->alterColumn('tr_pos_room_types', 'id_external', $this->integer()->notNull()->unique());
+        $this->alterColumn('tr_pos_room_types', 'id_external', $this->string(36)->notNull()->unique());
         $this->alterColumn('vacation_package', 'vp_external_id', $this->integer()->notNull()->unique());
     }
 
