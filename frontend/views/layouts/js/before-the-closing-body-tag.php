2125009f55971f5463eca3072eb8b2d21c5adc16
Vladimir Alekseev <vladimir.alekseev@gointeractive.co>
2026-03-03 17:24:55 +0200

    change chattilive.ai

--- frontend/views/layouts/js/before-the-closing-body-tag.php
+++ frontend/views/layouts/js/before-the-closing-body-tag.php
@@ -3,7 +3,7 @@ if (YII_ENV === "prod") {
     $this->registerJs('
     const script = document.createElement("script");
     script.async = true;
-    script.src = "https://get.chattilive.ai/widgets/js/a1364392-b0d7-408f-af59-1261d4985e2f";
+    script.src = "https://get.chattilive.ai/widgets/js/a1364e54-a1bf-4331-b2af-bbdce0d7254d";
     script.setAttribute("data-settings", \'{"debug":false}\');
     document.body.appendChild(script);
     ');
